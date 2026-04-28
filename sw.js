491b1391fc2dc7d345f0d8bcaf458266c1dc08b3
mbengueabdoulatif52-ai <mbengueabdoulatif52@gmail.com>
2026-04-28 17:11:14 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fiinas-v3';
+const CACHE = 'fiinas-v4';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
